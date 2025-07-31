8f80f93aaf0a39d386a2e0214d7d3c84c6ddb682
Saje Yrid <hray255@evit.edu>
2025-07-31 10:34:01 -0700

    door stuck

--- al.py
+++ al.py
@@ -1,33 +1,62 @@
-doorlocked = True
-doorclosed = True
 doorbroken = False
 playerpos = 0
-while playerpos == 0:
+while doorbroken == False:
     player = input("There is a door here. \n")
 
     if player == "look north":
         print("The same thing as usual.")
 
-    elif player == 'open door' and doorlocked == True:
+    elif player == "check door":
+        print("Just a door. Nothing special.")
+
+    elif player == "look west":
+        print("nothing.")
+
+    elif player == "look east":
+        print("nothing.")
+
+    elif player == "look south":
+        print("You aren't an owl, are you?")
+
+    elif player == 'open door':
         print("You jiggle the handle. the door is locked.")
-    
-    elif player == 'open door' and doorlocked == False and doorclosed == True:
-        print("You open the door. There is nothing here but a brick wall.")
-        doorclosed = False
-    
-    elif player == 'open door' and doorclosed == False:
-        print('The door is already open.')
     
     elif player == 'break door':
         print("You broke down the door. There is nothing beyond the frame but a brick wall.")
         doorbroken = True
-        doorclosed = False
-    
-    elif player == 'break door' and doorbroken == True:
-        print("It's already broken.")
 
     elif player == 'move forward' and doorclosed == True:
         print('You bang your head against the door.')
+    
+    else:
+        print("Invalid command.")
+
+while playerpos == 0:
+    player = input("There is no longer a door here. \n")
+
+    if player == "check door":
+        print("Nothing but splinters.")
+
+    elif player == "look north":
+        print("A brick wall standing in a doorframe.")
+
+    elif player == "look west":
+        print("Nothing.")
+
+    elif player == "look east":
+        print("Nothing.")
+
+    elif player == "look south":
+        print("You aren't an owl, are you?")
+    
+    elif player == 'open door':
+        print("You can't open splinters.")
+    
+    elif player == 'open door' and doorclosed == False:
+        print('The door is already open.')
+
+    elif player == 'break door' and doorbroken == True:
+        print("It's already broken.")
 
     elif player == 'move forward' and doorclosed == False:
         print('You pass through brick like water.')
@@ -35,4 +64,3 @@ while playerpos == 0:
     
     else:
         print("Invalid command.")
-    
