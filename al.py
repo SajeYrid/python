6de60f8c92e874b74febd26ee42c4d7cd12bb327
Saje Yrid <hray255@evit.edu>
2025-07-31 10:22:29 -0700

    Update al.py

--- al.py
+++ al.py
@@ -2,13 +2,13 @@ doorlocked = True
 doorclosed = True
 doorbroken = False
 playerpos = 0
-while playerpos != 1:
+while playerpos == 0:
     player = input("There is a door here. \n")
 
     if player == "look north":
-        print()
+        print("The same thing as usual.")
 
-    if player == 'open door' and doorlocked == True:
+    elif player == 'open door' and doorlocked == True:
         print("You jiggle the handle. the door is locked.")
     
     elif player == 'open door' and doorlocked == False and doorclosed == True:
@@ -16,22 +16,15 @@ while playerpos != 1:
         doorclosed = False
     
     elif player == 'open door' and doorclosed == False:
-        print('the door is already open.')
+        print('The door is already open.')
     
     elif player == 'break door':
-        print("you broke down the door. There is nothing beyond the frame but a brick wall.")
+        print("You broke down the door. There is nothing beyond the frame but a brick wall.")
         doorbroken = True
         doorclosed = False
     
     elif player == 'break door' and doorbroken == True:
         print("It's already broken.")
-    
-    elif player == 'unlock door':
-        print("you unlocked the door.")
-        doorlocked = False
-    
-    elif player == 'unlock door' and doorlocked == False:
-        print("the door is already unlocked.")
 
     elif player == 'move forward' and doorclosed == True:
         print('You bang your head against the door.')
@@ -41,5 +34,5 @@ while playerpos != 1:
         playerpos = 1
     
     else:
-        print("invalid command")
+        print("Invalid command.")
     
