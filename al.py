009d6240295a6329d1dbca2410bade31ffc99cf3
Saje Yrid <hray255@evit.edu>
2025-07-31 09:22:29 -0700

    Update al.py

--- al.py
+++ al.py
@@ -1,10 +1,14 @@
-doorlocked = True
+doorlocked = False
 player = input("There is a door here. \n")
 if player == 'open door' and doorlocked == True:
     print("You jiggle the handle. the door is locked.")
 elif player == 'open door' and doorlocked == False:
     print("You open the door. There is nothing here but a brick wall.")
 elif player == 'break door':
-    print("""you broke down the door. There is nothing beyond the frame but a brick wall.""")
+    print("you broke down the door. There is nothing beyond the frame but a brick wall.")
+elif player == 'unlock door':
+    print("you unlocked the door")
+    doorlocked = False
 else:
     print("invalid command")
+    
